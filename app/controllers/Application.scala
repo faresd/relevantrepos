250d36f0782be80175e6def302b97211c268cc1a
Fares DROUBI <fdr@zenexity.com>
2013-07-18 18:17:49 +0200

    Code refactoring Antoine Michel.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -51,23 +51,45 @@ object Application extends Controller {
 
 	def getCommitsByCollaborators(owner:String, rname:String) = Action {
 		Async {
-			var reposByUsersByCommits:Future[List[Map[String,List[String]]]] = githubWs.getCollaborators(owner, rname).flatMap (listCollaborators => {
-				val repositories:Future[List[Map[String,List[String]]]] = Future.sequence(listCollaborators.map (user => {
-					println("what is user?")
-					println(user)
-					val commitsByUser:Future[List[String]] = githubWs.getReposNameByUser(user).flatMap(l => { Future.sequence(l.map(repo => {
-						githubWs.getCommitsByUserAndRepos(user,repo)
-					}))}).map(_.flatten)
-					val commitsMapTolist:List[Future[(String, List[String])]] = Map(user -> commitsByUser).toList.map( key_value => key_value._2.map(xs => (key_value._1, xs) ))
+			// var reposByUsersByCommits:Future[List[Map[String,List[String]]]] = githubWs.getCollaborators(owner, rname).flatMap (listCollaborators => {
+			// 	val repositories:Future[List[Map[String,List[String]]]] = Future.sequence(listCollaborators.map (user => {
+			// 		val commitsByUser:Future[List[String]] = githubWs.getReposNameByUser(user).flatMap(l => { Future.sequence(l.map(repo => {
+			// 			githubWs.getCommitsByUserAndRepos(user,repo)
+			// 		}))}).map(_.flatten)
+			// 		val commitsMapTolist:List[Future[(String, List[String])]] = Map(user -> commitsByUser).toList.map( key_value => key_value._2.map(xs => (key_value._1, xs) ))
 
-					val commitsByUserAndRepos:Future[Map[String, List[String]]] = Future.sequence(commitsMapTolist).map(_.toMap)
-					commitsByUserAndRepos
-				}))
+			// 		val commitsByUserAndRepos:Future[Map[String, List[String]]] = Future.sequence(commitsMapTolist).map(_.toMap)
+			// 		commitsByUserAndRepos
+			// 	}))
 
-				repositories
-			})
+			// 	repositories
+			// })
 
-			val reposByUsersByCommitsToMap:Future[Map[String,List[String]]] = reposByUsersByCommits.map(i => listOfMapsToMap(i))
+			val collaborators: Future[List[String]] = githubWs.getCollaborators(owner, rname)
+
+			val repositoriesByCol: Future[List[(String, List[String])]] = collaborators.flatMap( collaborators => 
+				Future.sequence(collaborators.map { user =>
+					val repositories: Future[List[String]] = githubWs.getReposNameByUser(user)
+
+					repositories.map { repositories =>
+						user -> repositories
+					}
+			}))
+
+			val commitsByUserAndRepos: Future[List[(String, List[String])]] = repositoriesByCol.flatMap( repositoriesByCol => 
+				Future.sequence(repositoriesByCol.map { case (user, repos) =>
+					val commits: Future[List[String]] = Future.sequence(repos.map { repo =>
+						githubWs.getCommitsByUserAndRepos(user, repo)
+					}).map(_.flatten)
+
+					commits.map { commits =>
+						user -> commits
+					}
+			}))
+
+			val reposByUsersByCommitsToMap:Future[Map[String,List[String]]] = commitsByUserAndRepos.map(_.toMap)
+
+			//val reposByUsersByCommitsToMap:Future[Map[String,List[String]]] = reposByUsersByCommits.map(i => listOfMapsToMap(i))
 			reposByUsersByCommitsToMap.map(map => Ok(views.html.collaboratorsAndCommits(map)))
 
 		}
